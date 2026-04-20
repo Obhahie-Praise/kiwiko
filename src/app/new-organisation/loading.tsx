718be681a764469091f52b2d7d6f33d57a73b7b3
d-tech-guy <osemudiamhen.obhahie@upsshub.com>
2026-04-20 17:56:49 +0100

    rebuild: fresh Kiwiko restart

--- src/app/new-organisation/loading.tsx
+++
@@ -1,6 +0,0 @@
-
-import { FormSkeleton } from "@/components/common/Skeletons";
-
-export default function Loading() {
-  return <FormSkeleton />;
-}
--- src/constants/getSession.tsx
+++
@@ -1,9 +0,0 @@
-"use server"
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { cache } from "react";
-
-export const getSession = cache(async () => {
-  return auth.api.getSession({ headers: await headers() });
-});
-
